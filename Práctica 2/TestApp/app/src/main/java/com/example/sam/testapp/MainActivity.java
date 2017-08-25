34127da6b2178401b050f6cea0833ead4b1ffb80
Sam Magdaleno <larry_zot04@hotmail.com>
2017-08-25 09:35:11 -0700

    Subí práctica 2

---
+++ Práctica 2/TestApp/app/src/main/java/com/example/sam/testapp/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.sam.testapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        Button btn_click = (Button) findViewById(R.id.btnClick);
+        final EditText txt_name = (EditText) findViewById(R.id.txtName);
+
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        btn_click.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_LONG).show();
+                Snackbar.make(view, txt_name.getText(), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                Intent intentMain = new Intent(getApplicationContext(), SamActivity.class);
+                EditText editText = (EditText) findViewById(R.id.txtName);
+                String message = editText.getText().toString();
+                intentMain.putExtra("Mensaje", message);
+                startActivity(intentMain);
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
